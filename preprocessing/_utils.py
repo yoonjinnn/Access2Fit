9f57e64f5f1ca27507c91b4c4554d6774cab6d3d
yoonjinnn <123yoonnn@gmail.com>
2025-05-19 16:43:45 +0900

    Revert "add datapreprocess.py"
    This reverts commit 5b5bd1f04d2b6435664d3415740b9e839b116b5f.

--- preprocessing/_utils.py
+++
@@ -1,9 +0,0 @@
-import chardet
-
-def get_encoding(fpath, fname):
-    # 한글이 있어서 인코딩 형식 판단해주는 chardet 추가해봄
-    with open(fpath+fname, 'rb') as f:
-        result = chardet.detect(f.read(10000))  # 앞부분만 샘플링
-        # 출력예시
-        # {'encoding': 'EUC-KR', 'confidence': 0.99, 'language': 'Korean'}
-    return result
